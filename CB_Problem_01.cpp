6bfc2f7aea72208fbc8a4d9bdfe5f0d9665f2063
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-07-25 10:11:11 +0530

    Create CB_Problem_01.cpp

---
+++ CB_Problem_01.cpp
@@ -0,0 +1,19 @@
+//Today I am strating a C++ Pratice Set on Coding Buddies channel
+//Problem 1: Write a Program to print Hello World?
+
+#include<iostream>
+using namespace std;
+int main(){
+    //cout<<"Hello World";
+
+//Problem 2:Write a program to get input from user?
+  
+    string name;
+
+    cout << "Enter an name: ";
+    cin >> name;
+
+    cout << "You entered " << name;    
+    return 0;
+
+}
