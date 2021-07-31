110609a40632fd541d52d52eca2079d6dfc90be5
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-07-31 20:51:12 +0530

    Create Problem_2.cpp

---
+++ Problem_2.cpp
@@ -0,0 +1,11 @@
+//Problem 2: write a pragram to get input from user?
+
+#include<iostream>
+using namespace std;
+int main(){
+int Integer;
+cout<<"Enter the Integer :";
+cin>>Integer;
+cout<<Integer<<endl;
+return 0;
+}
