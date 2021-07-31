b16bb57a8c29e45e79e3e91d5ed8fd97d8418dc2
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-07-31 20:47:13 +0530

    Create CB_Problem_1.cpp

---
+++ 01_Input_Output_in_C_Plus_Plus/CB_Problem_1.cpp
@@ -0,0 +1,8 @@
+//Today I am strating a C++ Pratice Set on Coding Buddies channel
+//Problem 1: Write a Program to print Hello World?
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World";
+  return 0;
+}
