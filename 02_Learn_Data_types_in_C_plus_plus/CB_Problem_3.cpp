1e358d259ecd90188b761ef3e1119de71229a7c5
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-08-03 18:14:30 +0530

    Create CB_Problem_3.cpp

---
+++ 02_Learn_Data_types_in_C_plus_plus/CB_Problem_3.cpp
@@ -0,0 +1,12 @@
+//problem 1..
+#include<iostream>
+using namespace std;
+int main(){
+  // Problem 3..
+  cout<<"size of char :"<<sizeof(char)<<"bytes"<<endl;
+  cout<<"size of int :"<<sizeof(int)<<"bytes"<<endl;
+  cout<<"size of float :"<<sizeof(float)<<"bytes"<<endl;
+  cout<<"size of double :"<<sizeof(double)<<"bytes"<<endl;
+  return 0;
+}
+  
