c9a3415e0e31bc42244346a82b157455ae442cad
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-08-03 18:10:18 +0530

    Create CB_Problem_2.cpp

---
+++ 02_Learn_Data_types_in_C_plus_plus/CB_Problem_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Problem 2..
+    int dividend ,divisor,remainder,quotient;
+    cout<<"Enter dividend";
+   cin>>dividend;
+    cout<<"Enter divisor";
+    cin>>divisor;
+   quotient=dividend/divisor;
+   remainder=dividend%divisor;
+   cout<<"quotient ="<<quotient<<endl;
+   cout<<"remainder ="<<remainder<<endl;
+  return 0;
+}
+  
