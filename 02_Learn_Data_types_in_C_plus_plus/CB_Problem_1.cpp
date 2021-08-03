37f92d66a1c2a5a4a60194bf156e79d74cae698c
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-08-03 18:07:44 +0530

    Create CB_Problem_1.cpp

---
+++ 02_Learn_Data_types_in_C_plus_plus/CB_Problem_1.cpp
@@ -0,0 +1,12 @@
+//problem 1..
+#include<iostream>
+using namespace std;
+int main(){
+    int firstnumber,secondnumber,sumofnumber;
+    cout<<"Enter the Two integers";
+    cin>>firstnumber>>secondnumber;
+    sumofnumber=firstnumber+secondnumber;
+    cout<<firstnumber<<"+"<<secondnumber<<"="<<sumofnumber<<endl;
+  return 0;
+}
+  
