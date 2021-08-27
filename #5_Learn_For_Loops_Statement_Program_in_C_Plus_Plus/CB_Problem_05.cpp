84699960c27ae4341a397f53da5f09e7fae79004
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-08-27 10:14:05 +0530

    Create CB_Problem_05.cpp

---
+++ #5_Learn_For_Loops_Statement_Program_in_C_Plus_Plus/CB_Problem_05.cpp
@@ -0,0 +1,13 @@
+//Loops used for repeation
+//Syntax:-for(initialization,condition,updates){
+    //body of code
+//}
+//Problem_1:-Print the number from 1 to 5?
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        cout<<i<<"";
+    }
+  return 0;
+}
