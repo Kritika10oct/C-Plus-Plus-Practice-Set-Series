5de96211cefc768aec99046aaf9b9c5b74c15669
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-08-27 10:24:55 +0530

    Create CB_Problem_02.cpp

---
+++ #5_Learn_For_Loops_Statement_Program_in_C_Plus_Plus/CB_Problem_02.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Problem 2:-Display a text 5 times?
+    for(int i=1;i<=5;i++){
+        cout<<"Coding Buddies"<<endl;
+    }
+  return 0;
+}
