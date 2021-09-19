500832bd4a555d2184751d1a5b9cc05c20b9bce5
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-09-19 13:53:49 +0530

    Create CB_Problem_04.cpp

---
+++ #9_Practice_question_on_1-D_Array_in_C_plus_plus/CB_Problem_04.cpp
@@ -0,0 +1,16 @@
+//Array declaration:-
+//dataType arrayName[arraysize];
+#include<iostream>
+using namespace std;
+int main(){
+  //problem 4:-
+    int sum=0;
+    int arr[4]={12,45,60,78};
+    for(int i=0;i<4;i++){
+        sum +=arr[i];
+    }
+    cout<<sum<<endl;
+
+
+
+}
