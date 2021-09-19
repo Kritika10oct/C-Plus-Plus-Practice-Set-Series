6929ae85a973813dade1459be43e24319326b71b
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-09-19 13:49:42 +0530

    Create CB_Problem_02.cpp

---
+++ #9_Practice_question_on_1-D_Array_in_C_plus_plus/CB_Problem_02.cpp
@@ -0,0 +1,10 @@
+//Array declaration:-
+//dataType arrayName[arraysize];
+#include<iostream>
+using namespace std;
+int main(){
+//problem 2:-
+    int arr[4]={23,45,7,89};
+    for(int i=0;i<4;i++){
+         cout<<arr[i]<<"";
+    }
